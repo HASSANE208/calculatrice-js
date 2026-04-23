ac1a1596061f24009bde48b2b1530d45fddd88b4
KARIDIO HASSANE <hassanekaridiodaoudaidrissa@gmail.com>
2026-04-23 23:23:54 +0200

    initial: création de la calculatrice JS

---
+++ calculatrice.js
@@ -0,0 +1,15 @@
+// calculatrice.js
+// Calculatrice simple en JavaScript
+
+function additionner(a, b) { return a + b; }
+function soustraire(a, b) { return a - b; }
+function multiplier(a, b) { return a * b; }
+function diviser(a, b) {
+    if (b === 0) { throw new Error('Division par zéro impossible !'); }
+    return a / b;
+}
+
+console.log('5 + 3 =', additionner(5, 3));
+console.log('10 - 4 =', soustraire(10, 4));
+console.log('6 x 7 =', multiplier(6, 7));
+console.log('15 / 3 =', diviser(15, 3));
