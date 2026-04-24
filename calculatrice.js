bbd4c739b6bf82b60b06e0cacceb112d89d80b5c
moon boukar <saloufoubabarimounira@gmail.com>
2026-04-24 12:59:55 +0100

    feat: ajout de la fonction racine

--- calculatrice.js
+++ calculatrice.js
@@ -13,3 +13,15 @@ console.log('5 + 3 =', additionner(5, 3));
 console.log('10 - 4 =', soustraire(10, 4));
 console.log('6 x 7 =', multiplier(6, 7));
 console.log('15 / 3 =', diviser(15, 3));
+
+
+function racineCarree(n) {
+if (n < 0) {
+throw new Error('Impossible : racine carrée d un nombre négatif !');
+}
+return Math.sqrt(n);
+}
+console.log('√16 =', racineCarree(16));    
+// Attendu : 4
+console.log('√2 ≈', racineCarree(2));      
+// Attendu : 1.4142...
